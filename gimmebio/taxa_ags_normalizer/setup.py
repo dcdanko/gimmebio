125ea45c297bb5d192b4dd97d3c25b7270b13c33
David Danko <dcdanko@gmail.com>
2018-12-26 09:54:30 -0500

    cleaned up taxa ags normalizer microlib

--- gimmebio/taxa_ags_normalizer/setup.py
+++ gimmebio/taxa_ags_normalizer/setup.py
@@ -1,43 +1,11 @@
-import setuptools
-
-setuptools.setup(
-    name="taxa_ags_normalizer",
-    version="0.9.0",
-    url="https://github.com/dcdanko/taxa_ags_normalizer",
-
-    author="David C. Danko",
-    author_email="dcdanko@gmail.com",
-
-    description="Normalize Taxonomic Profiles By Average Genome Size",
-    long_description=open('README.rst').read(),
-
-    packages=['taxon_normalizer'],
-    package_dir={'taxon_normalizer': 'taxon_normalizer'},
-
-    install_requires=[
-        'click==6.7'
-    ],
-
-    entry_points={
-        'console_scripts': [
-            'taxon_normalizer=taxon_normalizer.cli:main'
-        ]
-    },
-
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-    ],
-)
 from setuptools import setup
 
-microlib_name = 'gimmebio.kmers'
+microlib_name = 'gimmebio.taxa_ags_normalizer'
 
 requirements = [
-    'gimmebio.seqs',
+    'numpy',
+    'pandas',
+    'scipy',
 ]
 
 setup(
