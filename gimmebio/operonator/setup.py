02365b1d2ae17f468ab31ecd19463012e853b746
David Danko <dcdanko@gmail.com>
2018-12-28 14:04:57 -0500

    clean up linting issues

--- gimmebio/operonator/setup.py
+++ gimmebio/operonator/setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup, find_packages
-from codecs import open
+from codecs import open as codex_open
 from os import path
 
 __version__ = '0.0.1'
@@ -7,11 +7,11 @@ __version__ = '0.0.1'
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with codex_open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 # get the dependencies and installs
-with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+with codex_open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
     all_reqs = f.read().split('\n')
 
 install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
