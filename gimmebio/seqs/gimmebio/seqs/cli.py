880d72a2a9db0c85bfae4fce8eb81c4d272dc844
David Danko <dcdanko@gmail.com>
2019-01-14 12:58:15 -0500

    fix bug

--- gimmebio/seqs/gimmebio/seqs/cli.py
+++ gimmebio/seqs/gimmebio/seqs/cli.py
@@ -97,7 +97,7 @@ def cli_concat_lanes(dryrun, dest_dir, filenames):
 @click.argument('file_2_out', type=click.File('w'))
 def cli_uninterleave_fastq(file_in, file_1_out, file_2_out):
     """Uninterleave a fastq file."""
-    for i, line in file_in:
+    for i, line in enumerate(file_in):
         i %= 8
         if i < 4:
             file_1_out.write(line)
