10b1e06e4a04038617520bf60767e2c830deeae0
David Danko <dcdanko@gmail.com>
2019-05-06 13:32:57 -0400

    Working Test code for SeqTalk
    No idea if code is performant but I can at least build and train a model

---
+++ gimmebio/seqtalk/tests/test_seqtalk.py
@@ -0,0 +1,50 @@
+"""Test suite for seqtalk."""
+
+from unittest import TestCase
+
+import tensorflow as tf
+from argparse import Namespace
+
+from gimmebio.seqtalk.autoencode.architecture import build_explicit_autoencoder
+from gimmebio.seqtalk.autoencode.training import train_autoencoder
+from gimmebio.seqtalk.seq_data import FastqSeqData
+
+EXAMPLE_FASTQ = '/Users/dcdanko/Dev/gimmebio/gimmebio/sample_seqs/gimmebio/sample_seqs/data/sample_fastq_zymo_control.fq'
+
+
+class TestSampleSeqs(TestCase):
+    """Test suite for sample seqs."""
+    '''
+    def test_build_model(self):
+        model = build_explicit_autoencoder(Namespace(
+            onehot_dims=9,
+            explicit_neurons=40,
+            input_length=128,
+            conv_filters=32,
+            conv_kernel_size=16,
+            alphabet_size=5,
+            learn_rate=tf.constant(1e-1),
+            temperature_tensor=tf.constant(1.0),
+            stochastic_tensor=tf.constant(True),
+        ))
+    '''
+
+    def test_training(self):
+        model = build_explicit_autoencoder(Namespace(
+            onehot_dims=9,
+            explicit_neurons=40,
+            dense_dims=100,
+            input_length=100,
+            conv_filters=32,
+            conv_kernel_size=16,
+            alphabet_size=4,
+            learn_rate=tf.constant(1e-1),
+            temperature_tensor=tf.constant(1.0),
+            stochastic_tensor=tf.constant(True),
+        ))
+        sess = tf.InteractiveSession()
+        sess.run(tf.global_variables_initializer())
+        data_source = FastqSeqData(EXAMPLE_FASTQ, seq_len=100)
+        print(model)
+        tr, va = train_autoencoder(model, sess, data_source, minibatch_size=50, num_epochs=1)
+
