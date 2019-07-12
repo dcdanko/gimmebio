0091c30008428a86e434d94d550aae2a282eb7ba
David Danko <dcdanko@gmail.com>
2019-07-12 11:54:55 -0400

    use gini coeff for clumpiness

--- gimmebio/entropy_scores/gimmebio/entropy_scores/entropy_scores.py
+++ gimmebio/entropy_scores/gimmebio/entropy_scores/entropy_scores.py
@@ -24,21 +24,30 @@ def entropy_score(bam_filename):
     return scores.mean()
 
 
+def gini_coeff(vals, nvals):
+    vals = sorted(list(vals))
+    total_val, total_area = 0, 0
+    for val in vals:
+        total_val += val
+        column = total_val / (nvals * len(vals))
+        total_area += column
+    return total_area / 0.5
+
+
 def clump_score(bam_filename, binsize=10000):
     samfile = pysam.AlignmentFile(bam_filename, "rb")
     return _clump_score(samfile, binsize=binsize)
 
 
 def _clump_score(samfile, binsize=10000):
-    nreads, bins = 0, set()
+    bins, nreads = {}, 0
     for read in samfile:
         if read.is_unmapped:
             continue
         nreads += 1
-        pos = read.reference_start // binsize
-        chrom = read.reference_id
-        bins.add((chrom, pos))
-    return len(bins) / nreads
+        pos = read.reference_id, read.reference_start // binsize
+        bins[pos] = 1 + bins.get(pos, 0)
+    return gini_coeff(bins.values(), nreads)
 
 
 def entropy_and_clump_score(bam_filename, binsize=10000):
